d7c1e3db7b975e925a42472e0010ce92c790e1f9
SE-Designs <dragonlorefnstattrack@gmail.com>
2024-01-26 16:23:53 +0600

    UPD: setup router, views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import CocktailView from '@/views/CocktailView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'cocktail-page',
+      component: CocktailView,
+      meta: {
+        title: 'Cocktail page'
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/:pathMatch(.*)*',
+      name: '404-page',
+      component: NotFoundView,
+      meta: {
+        title: 'Not found page'
+      }
     }
   ]
 })
